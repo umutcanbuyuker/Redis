61f3218b3eda1f587ff661d69a70b7a3420add15
Umut Can Buyuker <umutcandev@gmail.com>
2023-01-15 19:19:46 +0300

    Redis.NetCore

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 100 };
+            string jsonproduct = JsonConvert.SerializeObject(product);
+
+            // Byte çevirme ile set işlemleri
+            Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct);
+            _distributedCache.Set("product:1", byteproduct);
+
+            // JSON serialize işlemleri ile set leme işlemleri 
+            //await _distributedCache.SetStringAsync("product:1", jsonproduct, cacheEntryOptions);
+
+            /* async set ve set işlemleri
+            _distributedCache.SetString("names", "Fatih", cacheEntryOptions);
+            await _distributedCache.SetStringAsync("surname", "nayman");
+            */
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            // JSON serialize ile yapılan işlemin get edilmesi
+            //string jsonproduct = _distributedCache.GetString("product:1");
+
+            // Byte serialize ile yapılan işlemin get edilmesi
+            Byte[] byteproduct = _distributedCache.Get("product:1");
+            string jsonproduct = Encoding.UTF8.GetString(byteproduct);
+
+            // Byte ve Json da ortak olan kısım
+            Product p = JsonConvert.DeserializeObject<Product>(jsonproduct);
+            ViewBag.product = p;
+
+            //string name = _distributedCache.GetString("names");
+            //ViewBag.name = name;
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("names");
+
+            return View();
+        }
+
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/indir.jpg");
+
+            byte[] imagebyte = System.IO.File.ReadAllBytes(path);
+
+            _distributedCache.Set("resim", imagebyte);
+
+            return View();
+        }
+
+        public IActionResult ImageUrl()
+        {
+            byte[] resimbyte = _distributedCache.Get("resim");
+
+            return File(resimbyte, "image/jpg");
+        }
+    }
+}
---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        // ------------ Data cache leme için oluşturulacaklar. ------------
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        // ------------------------------------------------------------------
+
+        public IActionResult Index()
+        {
+            // Set edilecek key in memory de olup olmaması durumu kontrol ediliyor.
+            #region 11. Ders Memory Kontrol 1. YOL
+            /*
+            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString()); // Tarih bilgisini cache ye set ettim.
+            }
+            */
+            #endregion
+
+            // 2. YOL
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            //options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // 10 saniye sonra cache teki datayı siliyor.
+
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1); 
+
+            options.SlidingExpiration = TimeSpan.FromSeconds(10); // 10 saniye içerisinde dataya erişirsem data ömrü her seferinde 10 sn oluyor. Fakat bir üst satırda absolute olarak 1 dk girerek maksimum 60 saniye cache kalsın dedik. Bu sayede cache yi güncel tutabiliriz. 
+            // !! SlidingExpiration ile kesinlikle AbsoluteExpiration kullanılması önerilir.
+
+            // Cache Priority
+            options.Priority = CacheItemPriority.High; // Burada High, Low, Normal ve NeverRemove seçebiliyoruz. High önemli, Low önemsiz, Normal ortalama öncelik ve NeverRemove da asla demek. NeverRemove dersek ve yeni data eklersek; memory dolduğunda yeni cache eklersek exception atar.
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) => {
+                _memoryCache.Set("callback", $"{key} -> {value} => sebep: {reason}");
+            });
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options); // Tarih bilgisini cache ye set ettim. options ta ise cache e ömür ataması yapıyoruz.
+
+            Product p = new Product { Id = 1, Name = "Kalem", Price = 200 };
+
+            _memoryCache.Set<Product>("product:1", p); // Serialize işlemi otomatik olarak gerçekleşmiş oldu. Redis te bu işlem manuel olarak yapılacak.
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //_memoryCache.Remove("zaman"); // Cache içerisindeki zaman keyine sahip datayı memoryden siliyor.
+
+            #region GetOrCreate 11. ders
+            /*
+            _memoryCache.GetOrCreate<string>("zaman", entry => // GetorCreate mantığı girilen key e karşılık gelen data varsa al yoksa da bu key adında cache oluştur ve içerisinde fonksiyondaki datayı ata şeklindedir.
+            {
+                //entry yazma sebebimiz ise; eğer ki zaman veri tipine karşılık key yoksa key oluşturacak ve ekstra bir şeyler yapmak istersek entry üzerinden yapacağız.
+                return DateTime.Now.ToString();
+            });
+            */
+            #endregion
+
+            _memoryCache.TryGetValue("zaman", out string zamancache);
+            _memoryCache.TryGetValue("callback", out string callback);
+            ViewBag.callback = callback;
+            ViewBag.zaman = zamancache;
+
+            ViewBag.product = _memoryCache.Get<Product>("product:1");
+
+            //ViewBag.zaman = _memoryCache.Get<string>("zaman"); // Cache deki datayı key ismi üzerinden get ettim
+            return View();
+        }
+    }
+}
